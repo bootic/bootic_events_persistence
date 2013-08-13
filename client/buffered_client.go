3742f93c93d6a18adc8242af556645d42985e710
Ismael Celis <ismaelct@gmail.com>
2013-08-13 23:22:00 +0100

    Basic stat hat counter

---
+++ client/buffered_client.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+  "time"
+  data "github.com/bootic/bootic_go_data"
+  stathat "github.com/stathat/go"
+)
+
+type BufferedClient struct {
+  eventName string
+  stathatAccount string
+  Notifier data.EventsChannel
+  ticker *time.Ticker
+  count int
+}
+
+func (self *BufferedClient) Listen() {
+  for {
+    <- self.Notifier
+    // evtType, _     := event.Get("type").String()
+    self.increment()
+    // log.Println("Event!", evtType)
+  }
+}
+
+func (self *BufferedClient) increment() {
+  self.count = self.count + 1
+}
+
+func (self *BufferedClient) reset() {
+  self.count = 0
+}
+
+func (self *BufferedClient) post(count int) {
+  stathat.PostEZCount(self.eventName, self.stathatAccount, count)
+}
+
+func (self *BufferedClient) tick() {
+  for {
+    select {
+    case <- self.ticker.C:
+      if self.count != 0{
+        go self.post(self.count)
+        self.reset()
+      }
+    }
+  }
+}
+
+func NewBufferedClient(stathatAccount string, eventName string, duration time.Duration) (client *BufferedClient, err error) {
+  
+  client = &BufferedClient{
+    stathatAccount: stathatAccount,
+    eventName: eventName,
+    Notifier: make(data.EventsChannel, 1),
+    ticker: time.NewTicker(duration),
+  }
+  
+  go client.tick()
+  
+  return
+}
