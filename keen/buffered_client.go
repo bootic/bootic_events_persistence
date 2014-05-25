294a3998fd8a81d332e066b8322227142f9233c2
Ismael Celis <ismaelct@gmail.com>
2014-05-25 17:11:32 +0100

    POSTing events to KeenIO

--- keen/buffered_client.go
+++ keen/buffered_client.go
@@ -4,36 +4,116 @@ import (
   data "github.com/bootic/bootic_go_data"
   "time"
 	"log"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"fmt"
 )
 
+type StatsClient interface {
+	AddEvent(*data.Event)
+	Submit()
+}
+
+const(
+	KeenUrl = "https://api.keen.io/3.0/projects/%s/events?api_key=%s"
+)
+
+type Client struct {
+  projectId			string
+  apiKey				string
+	url						string
+	buffer				map[string][]interface{}
+	eventCount		int
+}
+
+type metaData struct {
+	Timestamp string `json:"timestamp"`
+}
+
+func (c *Client) AddEvent(event *data.Event) {
+	evtType, _  := event.Get("type").String()
+	evtTime, _  := event.Get("time").String()
+	evtPayload, _ := event.Get("data").Map()
+	evtPayload["keen"] = &metaData{evtTime}
+	c.buffer[evtType] = append(c.buffer[evtType], evtPayload)
+	c.eventCount = c.eventCount + 1
+}
+
+func (c *Client) Submit() {
+	if c.eventCount == 0 {
+		return
+	}
+
+	buff := c.buffer
+	evtCount := c.eventCount
+	c.reset()
+	jsonBytes, err := json.Marshal(buff)
+	if err != nil {
+		log.Fatal("Could not encode JSON", err)
+	}
+
+	resp, err := http.Post(c.url, "application/json", bytes.NewReader(jsonBytes))
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatal("Could not POST JSON", err)
+	}
+
+	log.Println(fmt.Sprintf("Sent %v events to KeenIo. Resp %v.", evtCount, resp.StatusCode))
+}
+
+func (c *Client) reset() {
+	c.buffer = make(map[string][]interface{})
+	c.eventCount = 0
+}
+
+func NewClient(projectId, apiKey string) (client *Client) {
+
+	client = &Client{
+		projectId: projectId,
+		apiKey: apiKey,
+		url: fmt.Sprintf(KeenUrl, projectId, apiKey),
+	}
+
+	client.reset()
+
+	return
+}
+
 type BufferedClient struct {
-  projectId      string
-  apiKey 				 string
-  Notifier       data.EventsChannel
-  ticker         *time.Ticker
+  Notifier			data.EventsChannel
+  ticker				*time.Ticker
+	clients				[]StatsClient
 }
 
 func (cl *BufferedClient) Listen() {
   for {
 		select {
 		case event := <- cl.Notifier:
-	    evtType, _     := event.Get("type").String()
-	    log.Println("Event!", evtType)
+			for i := range cl.clients {
+				cl.clients[i].AddEvent(event)
+			}
 		case <- cl.ticker.C:
-			log.Println("TICK!")
+			for i := range cl.clients {
+				go cl.clients[i].Submit()
+			}
 		}
     
   }
 }
 
+func (cl *BufferedClient) Register(c StatsClient) {
+	cl.clients = append(cl.clients, c)
+}
+
 func NewBufferedClient(projectId, apiKey string, duration time.Duration) (client *BufferedClient, err error) {
 
   client = &BufferedClient{
-    projectId: projectId,
-    apiKey: apiKey,
     Notifier: make(data.EventsChannel, 1),
     ticker: time.NewTicker(duration),
   }
+
+	client.Register(NewClient(projectId, apiKey))
 
   return
 }
